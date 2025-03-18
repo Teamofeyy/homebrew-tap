8704b394264d50efb9d2227112b40ba843a42afc
Teamofeyy <timarnd06@gmail.com>
2025-03-17 21:03:49 -0400

    Minor fixes

--- Formula/taskman.rb
+++ Formula/taskman.rb
@@ -1,8 +1,8 @@
 class Taskman < Formula
     desc "Simple bash task manager"
     homepage "https://github.com/Teamofeyy/Taskman"
-    url "https://github.com/Teamofeyy/Taskman/archive/refs/tags/v1.0.tar.gz"
-    sha256 "b039ed240e9836b870bd816adfeadffa2ef7c29ecd0a0993676b89efbb6dd816" # Замените реальным значением
+    url "https://github.com/Teamofeyy/Taskman/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "155d84f2f2484c41f385fb1689d710b500867c18dd45007652993db512cc266a" # Замените реальным значением
     license "MIT"
   
     def install
